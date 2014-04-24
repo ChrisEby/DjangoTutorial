c76203d5a2ea72eeb00da636a2c48866bbd7a0ad
ChrisEby <binarycorruption+github@plainbored.com>
2014-04-24 18:34:50 -0400

    Updating the polls site w/ the new models and also setting up the templates directory so I don't need to worry about it later.

--- DjangoTutorial/settings.py
+++ DjangoTutorial/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
@@ -37,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'MySite',
+    'polls',
 )
 
 MIDDLEWARE_CLASSES = (
