1f63f4fbf63e56f55b9e4e60ff4b47d8d6e0a753
ChrisEby <binarycorruption+github@plainbored.com>
2014-05-19 18:12:07 -0400

    Changing over to using the generic templates.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,28 @@
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.core.urlresolvers import reverse
+from django.views import generic
+from django.utils import timezone
 
 
-def index(request):
-    return HttpResponse("Index")
+class IndexView(generic.ListView):
+    template_name = 'blog/index.html'
+    context_object_name = 'latest_blog_posts'
+
+    def get_queryset(self):
+        """
+        TODO: Return back the latest blog posts
+        """
+        return HttpResponse("Index")
+
+
+class DetailView(generic.DetailView):
+    #model = Question
+    template_name = 'blog/detail.html'
+
+    def get_queryset(self):
+        """
+        TODO: Excludes any blog posts that aren't published yet.
+        """
+        #return Question.objects.filter(pub_date__lte=timezone.now())
+        return HttpResponse("Detail")
